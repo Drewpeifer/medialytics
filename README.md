07c6ba2a3187758a156fe44e6f14daa86733d0eb
Drew Peifer <dpeifer717@gmail.com>
2020-01-05 08:35:29 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# medialytics
-A basic, free tool to that shows information about Plex Media Server content
+# Medialytics
+### An SPA that displays the XML output of a Plex server in a nice format
+
+# How do I use it?
+
+* Replace the ip address in `scripts.js` with your server's public IP address
+* Replace the instances of `YOUR_TOKEN` with your own plex token ([instructions on locating token](https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/))
+  * **Do not share this value with anyone!**
+* Drag `index.html` into a browser
+
+**Please note:**
+* You may need to modify the structure of the XML path to match your server, e.g. library ID
+* This is built to display info for 2 libraries, Movies and TV. Modifying for more libraries should be a matter of copy/paste/edit.
+
+# How does it work?
+[Plex](http://www.plex.tv) servers typically contain a sizable library of awesome movies and TV shows. You can
+access the metadata for all that media via an XML feed that the server generates.
+This application calls that XML feed, parses it, and displays it in a format that's
+easy to filter and sort. I also convert that feed to JSON for use with the visualizations (charts).
+
+![visualizations](https://i.imgur.com/9T3tiNQ.png)
+
+![libraries](https://i.imgur.com/I73CQBb.jpg)
+
+# How recent is this data?
+Data is retrieved from Plex in real time. When you load the page, you are making a fresh request to my server.
+
+# What is it built with?
+jQuery does most of the work making the AJAX call and parsing the results. The sorting
+and filtering logic, as well as the accompanying animations, are all controlled by
+an awesome library called [Isotope](https://isotope.metafizzy.co).
+Charts are built using [D3](https://d3js.org/)/[C3](https://c3js.org/).
+
+# Why is some data missing?
+Plex pulls all its metadata from external agents like [theTVDB.com](http://thetvdb.com) which are
+crowd-sourced information. Some of the more obscure titles have incomplete data,
+especially when it comes to ratings, and I have not bothered to handle all the edge
+cases yet so you'll still see a few 'undefined' values floating around.
+
+# Warning:
+This is a WIP / pet project. Anyone is welcome to fork / contribute / use for non-commercial purposes.
+This application also relies upon using the private Plex token of your server, which you do **not** want to share
+with anyone (a malicious user can leverage it to access your server as an administrator).
---
+++ js/scripts.js
@@ -0,0 +1,445 @@
+// Instructions:
+// Replace IP address with your public server address,
+// Replace token with your server token ** KEEP PRIVATE **
+// Modify any library information to match your server's XML payload (e.g. libary ID's)
+/////////////
+
+// this function is fed a url and retrieves an XML payload
+jQuery.extend({
+    getPayload: function(url) {
+        var payload = null;
+        $.ajax({
+            url: url,
+            type: "GET",
+            dataType: "xml",
+            async: false,
+            timeout: 5000,
+            beforeSend: function() {
+                // call starts
+            },
+            complete: function() {
+                // call ends (before success/error)
+            },
+            success: function(data) {
+                console.log('ajax success');
+                payload = data;
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+              $('.alert').html('<p>Sorry!</p><p>The server must be down right now, or you\'re behind a firewall, or your browser doesn\'t like unsecured requests.</p>');
+              console.log('Error = ' + textStatus + '! The media server must be down right now, sorry.');
+            }
+        });
+       return payload;
+    }
+});
+
+// render movie charts on page load
+function renderMovieCharts(jsonData) {
+    var movieCount = jsonData.MediaContainer.Video.length,
+        releaseDateList = [],
+        releaseDateCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        decadePrefixes = ["193", "194", "195", "196", "197", "198", "199", "200", "201"],
+        decades = ["1930s", "1940s", "1950s", "1960s", "1970s", "1980s", "1990s", "2000s", "2010s"],
+        studioList = [];
+
+    // loop through movies and gather important data
+    $.each(jsonData.MediaContainer.Video, function() {
+        // track year
+        releaseDateList.push(this['@attributes'].year);
+        // track studio
+        studioList.push(this['@attributes'].studio);
+    });
+
+    // movies by decade chart
+    $.each(releaseDateList, function() {
+        // TODO: find a better way to do the following...
+        // sort through the releaseDateList,
+        // and depending on which decade prefix matches the year value,
+        // increment the corresponding releaseDateCount
+        var yearSub = this.substring(0, 3);
+
+        if (yearSub == decadePrefixes[0]) {
+            releaseDateCounts[0]++;
+        } else if (yearSub == decadePrefixes[1]) {
+            releaseDateCounts[1]++;
+        } else if (yearSub == decadePrefixes[2]) {
+            releaseDateCounts[2]++;
+        } else if (yearSub == decadePrefixes[3]) {
+            releaseDateCounts[3]++;
+        } else if (yearSub == decadePrefixes[4]) {
+            releaseDateCounts[4]++;
+        } else if (yearSub == decadePrefixes[5]) {
+            releaseDateCounts[5]++;
+        } else if (yearSub == decadePrefixes[6]) {
+            releaseDateCounts[6]++;
+        } else if (yearSub == decadePrefixes[7]) {
+            releaseDateCounts[7]++;
+        } else if (yearSub == decadePrefixes[8]) {
+            releaseDateCounts[8]++;
+        } else {
+            // date falls outside range
+            console.log('date out of range');
+        }
+    });
+    releaseDateCounts.unshift("releaseDateCounts");
+    c3.generate({
+        bindto: '.movies-by-decade',
+        x: 'x',
+        data: {
+            columns: [
+                releaseDateCounts
+            ],
+            type: 'bar'
+        },
+        axis: {
+            x: {
+                type: 'category',
+                categories: decades
+            }
+        },
+        legend: {
+            hide: true
+        },
+        color: {
+            pattern: ['#D62828', '#F75C03', '#F77F00', '#FCBF49', '#EAE2B7']
+        }
+    });
+
+    // movies by studio chart
+    var studioInstances = {},
+        studios = [];
+    // count how many times each studio occurs in studioList,
+    // and build a dictionary from results
+    for (var i = 0, j = studioList.length; i < j; i++) {
+       studioInstances[studioList[i]] = (studioInstances[studioList[i]] || 0) + 1;
+    }
+    // split dictionary into two arrays
+    for (var prop in studioInstances) {
+       if (!studioInstances.hasOwnProperty(prop)) {
+          continue;
+       }
+       studios.push([prop, studioInstances[prop]]);
+    }
+
+    c3.generate({
+        bindto: '.movies-by-studio',
+        data: {
+            columns: studios.slice(0, 50),
+            type : 'pie'
+        },
+        pie: {
+            label: {
+                format: function (value, ratio, id) {
+                    return value;
+                }
+            }
+        },
+        color: {
+            pattern: ['#D62828', '#F75C03', '#F77F00', '#FCBF49', '#EAE2B7']
+        },
+        legend: {
+            hide: true
+        },
+        tooltip: {
+            format: {
+                value: function (value, ratio, id) {
+                    return id + ' : ' + value;
+                }
+            }
+        }
+    });
+}
+
+// render TV charts on page load
+function renderTVCharts(jsonData) {
+    var showCount = jsonData.MediaContainer.Directory.length,
+        releaseDateList = [],
+        releaseDateCounts = [0, 0, 0, 0, 0, 0],
+        decadePrefixes = ["196", "197", "198", "199", "200", "201"],
+        decades = ["1960s", "1970s", "1980s", "1990s", "2000s", "2010s"],
+        studioList = [];
+
+    // loop through TV and gather important data
+    $.each(jsonData.MediaContainer.Directory, function() {
+        // track year
+        releaseDateList.push(this['@attributes'].year);
+        // track studio
+        studioList.push(this['@attributes'].studio);
+    });
+
+    // TV Shows by decade chart
+    $.each(releaseDateList, function() {
+        // TODO: find a better way to do the following...
+        // sort through the releaseDateList,
+        // and depending on which decade prefix matches the year value,
+        // increment the corresponding releaseDateCount
+        if (typeof this === 'string' || this instanceof String) {
+            var yearSub = this.substring(0, 3);
+        } else {
+            yearSub = "undefined";
+        }
+
+        if (yearSub == decadePrefixes[0]) {
+            releaseDateCounts[0]++;
+        } else if (yearSub == decadePrefixes[1]) {
+            releaseDateCounts[1]++;
+        } else if (yearSub == decadePrefixes[2]) {
+            releaseDateCounts[2]++;
+        } else if (yearSub == decadePrefixes[3]) {
+            releaseDateCounts[3]++;
+        } else if (yearSub == decadePrefixes[4]) {
+            releaseDateCounts[4]++;
+        } else if (yearSub == decadePrefixes[5]) {
+            releaseDateCounts[5]++;
+        } else if (yearSub == decadePrefixes[6]) {
+            releaseDateCounts[6]++;
+        } else {
+            // date falls outside range
+            console.log('date undefined or out of range');
+        }
+    });
+    releaseDateCounts.unshift("releaseDateCounts");
+    c3.generate({
+        bindto: '.shows-by-decade',
+        x: 'x',
+        data: {
+            columns: [
+                releaseDateCounts
+            ],
+            type: 'bar'
+        },
+        axis: {
+            x: {
+                type: 'category',
+                categories: decades
+            }
+        },
+        legend: {
+            hide: true
+        },
+        color: {
+            pattern: ['#D62828', '#F75C03', '#F77F00', '#FCBF49', '#EAE2B7']
+        }
+    });
+
+    // TV shows by studio chart
+    var studioInstances = {},
+        studios = [];
+    // count how many times each studio occurs in studioList,
+    // and build a dictionary from results
+    for (var i = 0, j = studioList.length; i < j; i++) {
+       studioInstances[studioList[i]] = (studioInstances[studioList[i]] || 0) + 1;
+    }
+    // split dictionary into two arrays
+    for (var prop in studioInstances) {
+       if (!studioInstances.hasOwnProperty(prop)) {
+          continue;
+       }
+       studios.push([prop, studioInstances[prop]]);
+    }
+
+    c3.generate({
+        bindto: '.shows-by-studio',
+        data: {
+            columns: studios.slice(0, 50),
+            type : 'pie'
+        },
+        pie: {
+            label: {
+                format: function (value, ratio, id) {
+                    return value;
+                }
+            }
+        },
+        color: {
+            pattern: ['#D62828', '#F75C03', '#F77F00', '#FCBF49', '#EAE2B7']
+        },
+        legend: {
+            hide: true
+        },
+        tooltip: {
+            format: {
+                value: function (value, ratio, id) {
+                    return id + ' : ' + value;
+                }
+            }
+        }
+    });
+}
+
+// this grabs the appropriate payload depending on which input requests it
+// and then populates the UI with entries
+function renderGrid() {
+    var wrapper = $('.content'),
+    // set count to 0
+    count = 0,
+    // build URLs
+    serverUrl = 'http://123.456.78.910:12345',
+    token = 'X-Plex-Token=YOUR_TOKEN',
+    baseUrl = serverUrl + '/library/sections/all?' + token,
+    moviesUrl = serverUrl + '/library/sections/1/all?' + token,
+    showsUrl = serverUrl + '/library/sections/2/all?' + token,
+    recentlyAddedUrl = serverUrl + '/library/recentlyAdded/search?type=1&X-Plex-Container-Start=0&X-Plex-Container-Size=20&' + token,
+    urls = [showsUrl,moviesUrl];// hiding recentlyAddedUrl for now
+
+    // disable query button
+    $('.query').attr('disabled', 'disabled');
+
+    // for each entry in urls, grab XML
+    $.each(urls, function(i, url) {
+        wrapper = $('.content');
+        payload = $.getPayload(url);
+        // target the entries within the payload
+        target = payload.children[0].children;
+        // count items
+        i = target.length;
+        // target first entry for sample data
+        firstItem = $(payload.children[0].children[0]);
+        // store the name of the library being queried;
+        // recently added needs some massaging
+        if ($(payload.children[0]).attr('mixedParents') == 1) {
+            targetLibrary = 'Recently Added';
+            targetType = 'recent';
+        } else {
+            targetLibrary = 'All ' + $(payload.children[0]).attr('librarySectionTitle');
+            targetType = firstItem.attr('type');
+        }
+        // parse payload items and build each entry into the DOM
+        $(payload).find(target).each(function() {
+            // store data for each entry
+            entry = $(this),
+            name = entry.attr('title'),
+            sortTitle = entry.attr('titleSort'),
+            year = entry.attr('year'),
+            img = entry.attr('thumb'),
+            type = entry.attr('type'),
+            duration = entry.attr('duration'),
+            dateAdded = entry.attr('addedAt'),
+            ratingMPAA = entry.attr('contentRating'),
+            ratingAudience = entry.attr('audienceRating'),
+            imgUrl = serverUrl + img + '?' + token,
+            grid = $('.content .grid');
+
+            // again, massage data for recently added entries
+            if (targetType == 'recent') {
+                type = 'recent';
+            } else {
+            };
+
+            // build UI for each entry
+            entryInterface = $('<div data-datereleased="' + year + '" ' +
+                                'data-name="' + name + '" ' +
+                                'data-dateadded="' + dateAdded + '" ' +
+                                'data-sorttitle="' + sortTitle + '" ' +
+                                'data-duration="' + duration + '" ' +
+                                'data-src="' + imgUrl + '" ' +
+                                'class="entry ' + type + ' lazy">' +
+                                '<img class="entry-icon" src="img/' + type + '.jpg">' +
+                                '<p class="name">' + name + ' (' + year + ')</p>' +
+                                '<p class="rating-MPAA">Rated: ' + ratingMPAA + '</p>' +
+                                '<p class="rating-audience">Rotten Tomatoes: ' + ratingAudience + '</p>' +
+                                '</div>');
+            // append it to the target list, set background
+            entryInterface.appendTo(grid);
+        });
+    count = count + i;
+    });
+    // append header
+    $('.content h3').text('Total Entries: ' + count);
+    // there's a bug with lazy load and dynamic elements that
+    // means lazy load needs applied before the element renders,
+    // or else the background starts loading immediately. So,
+    // everything needs to be hidden (.content is set to display:none)
+    // while we lazy load images
+    $('.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        beforeLoad: function(element) {
+            // called before an elements gets handled
+        },
+        afterLoad: function(element) {
+            // called after an element was successfully handled
+        },
+        onError: function(element) {
+            // called whenever an element could not be handled
+        },
+        onFinishedAll: function() {
+            // called once all elements was handled
+        }
+    });
+    // reveal controls
+    $('.controls').css('display', 'inline-block');
+    // now that all entries are appended and have lazy load applied,
+    // reveal the entire grid of entries
+    $('.content').show();
+    // since the .content area was hidden (0 width) when the entries were appended,
+    // they all stack on top of each other, and now we need to
+    // initialize isotope on grid for a fresh sort, which re-aligns them
+    $('.grid').isotope({
+        itemSelector: 'div.entry',
+        layoutMode: 'fitRows',
+        getSortData: {
+            name: '.name',
+            ratingMPAA: '.rating-MPAA',
+            ratingAudience: '.rating-audience',
+            dateReleased: '[data-datereleased]',
+            dateAdded: '[data-dateadded]'
+        },
+        sortBy: 'name'
+    });
+    // another bug with lazy load, even though the .content div
+    // is visible, and so are the entries within it, and they've been
+    // shuffled into horizontal rows by isotope, lazy load still doesn't
+    // consider them "visible" until a scroll event is triggered,
+    // so we have to do this hacky thing:
+    $(window).scroll();
+}
+
+// bind the query buttons
+$('.query').click(renderGrid);
+// filtering
+$('button.filter').each(function() {
+    $(this).click(function() {
+        if ($(this).hasClass('active')) {
+            // do nothing
+        } else {
+            $('button.filter').removeClass('active');
+            $(this).addClass('active');
+            $('.grid').isotope({ filter: '.' + $(this).attr('data-filter') });
+            console.log('now filtering by ' + $(this).attr('data-filter'));
+        }
+    });
+});
+// sorting
+$('button.sort').each(function() {
+    $(this).click(function() {
+        if ($(this).hasClass('active')) {
+            if ($(this).hasClass('reverse-sort')) {
+                $('.grid').isotope({ sortAscending: true });
+                $(this).removeClass('reverse-sort');
+            } else {
+                $('.grid').isotope({ sortAscending: false });
+                $(this).addClass('reverse-sort');
+            }
+        } else {
+            $('button.sort').removeClass('active reverse-sort');
+            $(this).addClass('active');
+            $('.grid').isotope({ sortBy: $(this).attr('data-sort'), sortAscending: true });
+        }
+        console.log('now sorting by ' + $(this).attr('data-sort'));
+    });
+});
+// on load
+$(function() {
+    var movieData = $.getPayload('http://123.456.78.910:12345/library/sections/1/all?X-Plex-Token=YOUR_TOKEN'),
+        movieJson = xmlToJson(movieData),
+        tvData = $.getPayload('http://123.456.78.910:12345/library/sections/2/all?X-Plex-Token=YOUR_TOKEN'),
+        tvJson = xmlToJson(tvData);
+
+    renderMovieCharts(movieJson);
+    renderTVCharts(tvJson);
+    // late addition: prepend library counts to charts
+    $('.movies .c3').prepend('<p class="count">Total: ' + movieJson.MediaContainer.Video.length + '</p>');
+    $('.tv .c3').prepend('<p class="count">Total: ' + tvJson.MediaContainer.Directory.length + '</p>');
+});
