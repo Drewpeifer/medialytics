44e42d9ab88211bd4f3321bd7b4b4a7bb7f89e15
Drew Peifer <drew@drewpeifer.com>
2023-10-03 18:04:15 -0400

    formatting and cleanup, added longest/oldest stats

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,47 @@
 ////// WARNING
 // Never share the following token with anyone! Do not host this on a public server with the token in place!
 // Keep it secret, keep it safe! If compromised, generate a new one: https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
-const serverToken = 'YOUR_SERVER_TOKEN',// ex: 'ad2T-askdjasd9WxJVBPQ'
-    serverIp = 'YOUR_SERVER_IP',// ex: 'http://12.345.678.90:32400'
-    libraryListUrl = serverIp + '/library/sections?X-Plex-Token=' + serverToken,
-    // below are the limits for displaying data in the charts, e.g. "Top X Countries", and the recently added list
-    countryLimit = 20,
-    genreLimit = 20,
-    studioLimit = 20,
-    recentLimit = 10,
-    recentlyAddedUrl = serverIp + '/library/recentlyAdded/search?type=1&X-Plex-Container-Start=0&X-Plex-Container-Size=' + recentLimit + '&X-Plex-Token=' + serverToken,
-    // below are the decade arrays used for the items by decade chart, any data outside of these decades will
-    // be collected but not displayed by the charts. Explicitly stating these instead of computing for easier customization of charts
-    decadePrefixes = ["193", "194", "195", "196", "197", "198", "199", "200", "201", "202"],// used for comparing raw release years
-    decades = ["1930s", "1940s", "1950s", "1960s", "1970s", "1980s", "1990s", "2000s", "2010s", "2020s"];// used for UI/chart display
+const serverToken = 'SERVER_TOKEN',// ex: 'ad2T-askdjasd9WxJVBPQ'
+serverIp = 'SERVER_IP',// ex: 'http://12.345.678.90:32400'
+libraryListUrl = serverIp + '/library/sections?X-Plex-Token=' + serverToken,
+// below are the limits for displaying data in the charts, e.g. "Top X Countries", and the recently added list
+countryLimit = 20,
+genreLimit = 20,
+studioLimit = 20,
+recentLimit = 10,
+recentlyAddedUrl = serverIp + '/library/recentlyAdded/search?type=1&X-Plex-Container-Start=0&X-Plex-Container-Size=' + recentLimit + '&X-Plex-Token=' + serverToken,
+// below are the decade arrays used for the items by decade chart, any data outside of these decades will
+// be collected but not displayed by the charts. Explicitly stating these instead of computing for easier customization of charts
+decadePrefixes = ["193", "194", "195", "196", "197", "198", "199", "200", "201", "202"],// used for comparing raw release years
+decades = ["1930s", "1940s", "1950s", "1960s", "1970s", "1980s", "1990s", "2000s", "2010s", "2020s"];// used for UI/chart display
 
 // GLOBAL VARIABLES
 let debugMode = false,// set to true to enable console logging
-    availableLibraries = [],// the list of libraries returned by your server
-    selectedLibrary = "",// the library currently selected by the user
-    selectedLibraryKey = "",// the key of the library currently selected by the user
-    selectedLibraryStats = {},// a large object containing all the stats for the selected library
-    selectedLibrarySummary = "",// plain text summary of stat highlights
-    libraryStatsLoading = false,// used to trigger loading animations
-    recentlyAdded = [],// the list of recently added items returned by your server
-    countries = {},// this stores country: count, and is then split into the two following arrays for the bar chart
-    countryList = [],
-    countryCounts = [],
-    releaseDateList = [],// stores each instance of a release date
-    releaseDateCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],// stores count of decades within releaseDateList (matched against decadePrefixes array for comparison)
-    studioInstances = [],
-    sortedStudios = [],
-    genres = {},// this stores genre: count, and is then split into the two following arrays
-    genreList = [],
-    genreCounts = [],
-    durationSum = 0,// aggregate duration of all movies, or total duration of all shows (# of episodes * avg episode duration)
-    seasonSum = 0,
-    episodeCounts = []
-    episodeSum = 0;
+availableLibraries = [],// the list of libraries returned by your server
+selectedLibrary = "",// the library currently selected by the user
+selectedLibraryKey = "",// the key of the library currently selected by the user
+selectedLibraryStats = {},// a large object containing all the stats for the selected library
+selectedLibrarySummary = "",// plain text summary of stat highlights
+libraryStatsLoading = false,// used to trigger loading animations
+recentlyAdded = [],// the list of recently added items returned by your server
+countries = {},// this stores country: count, and is then split into the two following arrays for the bar chart
+countryList = [],
+countryCounts = [],
+releaseDateList = [],// stores each instance of a release date
+releaseDateCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],// stores count of decades within releaseDateList (matched against decadePrefixes array for comparison)
+oldestTitle = "",// the oldest title in the library
+oldestReleaseDate = "",// the oldest release date in the library
+studioInstances = [],
+sortedStudios = [],
+genres = {},// this stores genre: count, and is then split into the two following arrays
+genreList = [],
+genreCounts = [],
+durationSum = 0,// aggregate duration of all movies, or total duration of all shows (# of episodes * avg episode duration)
+longestDuration = 0,// longest duration of a movie, or longest show (# of episodes)
+longestTitle = "",// title of item with longest duration / episode count
+seasonSum = 0,
+episodeCounts = []
+episodeSum = 0;
 
 /////////////////////////////////
 // gets list of available libraries
@@ -94,6 +98,8 @@ const resetLibraryStats = () => {
     countryCounts = [],
     releaseDateList = [],
     releaseDateCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    oldestTitle = "",
+    oldestReleaseDate = "",
     studioInstances = [],
     sortedStudios = [],
     genres = {},
@@ -102,19 +108,27 @@ const resetLibraryStats = () => {
     durationSum = 0,
     seasonSum = 0,
     episodeCounts = []
-    episodeSum = 0;
+    episodeSum = 0,
+    longestDuration = 0,
+    longestTitle = "";
 }
 
 /////////////////////////////////
 // parse through a media payload
 const parseMediaPayload = (data) => {
     let itemCount = data.data.MediaContainer.size,
-        type = data.data.MediaContainer.viewGroup;
+    type = data.data.MediaContainer.viewGroup;
     
     // loop through items and gather important data
     data.data.MediaContainer.Metadata.forEach((item, index) => {
         // track year
         releaseDateList.push(item.year);
+
+        // track oldest release date
+        if (oldestTitle == "" || new Date(item.originallyAvailableAt) < new Date(oldestReleaseDate)) {
+            oldestTitle = item.title + ' (' + item.originallyAvailableAt + ')';
+            oldestReleaseDate = item.originallyAvailableAt;
+        }
         // track studio
         studioInstances.push(item.studio);
         // track durations
@@ -130,6 +144,19 @@ const parseMediaPayload = (data) => {
         } else {
             // it's a movie
             durationSum = durationSum + (item.duration/60000);
+        }
+        // track longest duration (runtime for movie, number of episodes for tv)
+        if (item.type === 'movie') {
+            if (longestDuration === 0 || item.duration > longestDuration) {
+                longestDuration = item.duration;
+                longestTitle = item.title;
+            }
+        } else {
+            // it's a tv show
+            if (longestDuration === 0 || item.leafCount > longestDuration) {
+                longestDuration = item.leafCount;
+                longestTitle = item.title;
+            }
         }
         // track genres
         if (item.Genre) {
@@ -166,10 +193,10 @@ const parseMediaPayload = (data) => {
         // (bar charts want 2 arrays of values, while pie charts want a dictionary)
         if (index == itemCount - 1) {
             let totalMins = Math.round(durationSum),
-                totalHours = Math.floor(durationSum/60),
-                totalDays = Math.floor(durationSum/24/60),
-                displayHours = totalHours - (totalDays*24),
-                displayMins = totalMins - (totalHours*60);
+            totalHours = Math.floor(durationSum/60),
+            totalDays = Math.floor(durationSum/24/60),
+            displayHours = totalHours - (totalDays*24),
+            displayMins = totalMins - (totalHours*60);
 
             //////////////////////////
             // items by country chart
@@ -227,7 +254,7 @@ const parseMediaPayload = (data) => {
             });
 
             let topDecade = decades[releaseDateCounts.indexOf(Math.max(...releaseDateCounts))],
-                topDecadeCount = Math.max(...releaseDateCounts).toLocaleString();
+            topDecadeCount = Math.max(...releaseDateCounts).toLocaleString();
 
             ////////////////////////
             // items by studio chart
@@ -268,32 +295,35 @@ const parseMediaPayload = (data) => {
                 topCountryCount: countryCounts[1].toLocaleString(),
                 topDecade: topDecade,
                 topDecadeCount: topDecadeCount,
+                oldestTitle: oldestTitle,
                 studios: studios,
                 topStudio: sortedStudios[0][0],
                 topStudioCount: sortedStudios[0][1].toLocaleString(),
                 type: type,
-                increment: type === 'movie'? 'movies' : 'shows',
+                increment: type === 'movie'? 'movie' : 'show',
                 totalDuration: totalDays + " Days, " + displayHours + " Hours and " + displayMins + " Mins",
                 seasonSum: seasonSum,
                 episodeSum: episodeSum,
                 studioLimit: studioLimit,
                 countryLimit: countryLimit,
-                genreLimit: genreLimit
+                genreLimit: genreLimit,
+                longestDuration : longestDuration,
+                longestTitle : longestTitle,
             }
 
             // set concatenated summary string
             app.selectedLibrarySummary = type === 'movie' ?
-                // movies
-                `This library contains ${app.selectedLibraryStats.totalItems.toLocaleString()}
-                ${app.selectedLibraryStats.increment} produced by ${ Object.keys(app.selectedLibraryStats.studios).length.toLocaleString()} studios
-                across ${Object.keys(countries).length.toLocaleString()} countries spanning ${Object.keys(genres).length.toLocaleString()}
-                genres. The total duration is ${app.selectedLibraryStats.totalDuration}.` :
-                // tv
-                `This library contains ${app.selectedLibraryStats.totalItems.toLocaleString()} ${app.selectedLibraryStats.increment}
-                (${app.selectedLibraryStats.seasonSum.toLocaleString()} seasons / ${app.selectedLibraryStats.episodeSum.toLocaleString()} episodes)
-                produced by ${Object.keys(app.selectedLibraryStats.studios).length.toLocaleString()} studios across
-                ${Object.keys(countries).length.toLocaleString()} countries spanning ${Object.keys(genres).length.toLocaleString()} genres.
-                The total duration is ${app.selectedLibraryStats.totalDuration}.`
+            // movies
+            `This library contains ${app.selectedLibraryStats.totalItems.toLocaleString()}
+            ${app.selectedLibraryStats.increment}s produced by ${ Object.keys(app.selectedLibraryStats.studios).length.toLocaleString()} studios
+            across ${Object.keys(countries).length.toLocaleString()} countries spanning ${Object.keys(genres).length.toLocaleString()}
+            genres. The total duration is ${app.selectedLibraryStats.totalDuration}.` :
+            // tv
+            `This library contains ${app.selectedLibraryStats.totalItems.toLocaleString()} ${app.selectedLibraryStats.increment}s
+            (${app.selectedLibraryStats.seasonSum.toLocaleString()} seasons / ${app.selectedLibraryStats.episodeSum.toLocaleString()} episodes)
+            produced by ${Object.keys(app.selectedLibraryStats.studios).length.toLocaleString()} studios across
+            ${Object.keys(countries).length.toLocaleString()} countries spanning ${Object.keys(genres).length.toLocaleString()} genres.
+            The total duration is ${app.selectedLibraryStats.totalDuration}.`
 
             // render charts
             renderCharts();
@@ -306,7 +336,6 @@ const parseMediaPayload = (data) => {
             }
         }
     });
-
 }
 
 const renderCharts = () => {
